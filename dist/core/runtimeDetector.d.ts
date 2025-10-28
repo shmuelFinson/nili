fe5a42acbd86354cbfe94dcdad84d84e73c84d02
shmuelFinson <shmuelfinson@gmail.com>
2025-10-28 21:36:40 +0200

    Remove VS Code and build folders from repo

--- dist/core/runtimeDetector.d.ts
+++
@@ -1,3 +0,0 @@
-export type Runtime = "node" | "python" | "go" | "rust" | "java" | "ruby" | "unknown";
-export declare function detectRuntime(cwd?: string): Runtime;
-//# sourceMappingURL=runtimeDetector.d.ts.map
